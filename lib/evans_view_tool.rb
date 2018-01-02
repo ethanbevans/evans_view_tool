6715055bacd0ed10ec79bc9ac6e127ae751995e3
Ethan Evans <ethanbevans@gmail.com>
2018-01-02 04:30:16 -0500

    Evans View Tool Initial Commit

---
+++ lib/evans_view_tool.rb
@@ -0,0 +1,7 @@
+require "evans_view_tool/version"
+require "evans_view_tool/renderer"
+
+
+module EvansViewTool
+  # Your code goes here...
+end
---
+++ lib/evans_view_tool/version.rb
@@ -0,0 +1,3 @@
+module EvansViewTool
+  VERSION = "0.1.0"
+end
